4c9d3dd6ba2ecd1864dd622639e1a897459b4a42
UGO <ugos9628@gmail.com>
2025-10-19 12:26:56 +0100

    stmp back

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,26 +1,32 @@
 // src/services/emailService.ts
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465, // use SSL port
+  secure: true, // must be true for port 465
+  auth: {
+    user: "ajayisegun2003@gmail.com",
+    pass: "spudyopyvqaljvmy",
+  },
+   pool: true, // reuses connection
+  maxConnections: 2,
+  maxMessages: 30,
+  connectionTimeout: 20000, // 20 seconds
+  socketTimeout: 20000,
+});
 
 export async function sendEmail(to: string, subject: string, html: string) {
   try {
-    const { data, error } = await resend.emails.send({
-      from: "Proxy <onboarding@resend.dev>", // use a verified domain or default
+    await transporter.sendMail({
+      from: `"Proxy" <${process.env.SMTP_USER}>`,
       to,
       subject,
       html,
     });
-
-    if (error) {
-      console.error("❌ Email failed:", error);
-      throw new Error("Failed to send email");
-    }
-
     console.log(`✅ Email sent to ${to}`);
-    return data;
   } catch (error) {
-    console.error("❌ Email send error:", error);
+    console.error("❌ Email failed:", error);
     throw new Error("Failed to send email");
   }
 }
