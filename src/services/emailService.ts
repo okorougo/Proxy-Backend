716c2f3d6bcbc39ab7a87dc9cf27a2c128f3f9d9
UGO <ugos9628@gmail.com>
2025-10-22 08:36:22 +0400

    gmail

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -2,9 +2,9 @@ import { google } from "googleapis";
 import dotenv from "dotenv";
 dotenv.config();
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
-const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN!;
+const CLIENT_ID = process.env.GOOGLE_ID!;
+const CLIENT_SECRET = process.env.GOOGLE_SECRET!;
+const REFRESH_TOKEN = process.env.GOOGLE_REFRESHTOKEN!;
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
