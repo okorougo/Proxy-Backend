8191371f752b3a0b6c3b19aafe36e2bd68c4765d
UGO <ugos9628@gmail.com>
2025-11-23 12:33:41 +0100

    as how na

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
 export default prisma;
