7a8b2589a35758f145cc497b0a4224d5b9d97fc4
UGO <ugos9628@gmail.com>
2025-11-23 17:40:12 +0100

    how na

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,14 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 import "dotenv/config";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg(pool),
-});
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
 export default prisma;
